ac9d586b40e14335df6967c4dac845440172b16f
Balveer Singh <balveersinghyt@gmail.com>
2021-03-08 18:30:25 +0530

    Update Machine_Error_Report.ino

--- Machine_Error_Report/Machine_Error_Report.ino
+++ Machine_Error_Report/Machine_Error_Report.ino
@@ -8,6 +8,12 @@ int red_button = 8;
 int yellow_button = 9;
 int green_button = 10;
 
+// Press LED
+
+int red_press = A4;
+int yellow_press = A5;
+int green_press = 13;
+
 // 16*2 LCD...........
 int Contrast=95;
 LiquidCrystal lcd(12, 11, A0, A1, A2, A3);
@@ -25,10 +31,16 @@ int red = 4;
 int yellow = 5;
 int green = 7;
 
-int sms_count = 0;
+int sms_count1 = 0, sms_count2 = 0, sms_count3 = 0;
 
 
 void setup() {
+  //LED BLINK
+
+  pinMode(red_press, OUTPUT);
+  pinMode(yellow_press, OUTPUT);
+  pinMode(green_press, OUTPUT);
+  
   // LCD
   analogWrite(6,Contrast);
   lcd.begin(16, 2);
@@ -49,8 +61,7 @@ void setup() {
 
 void sendMessage(char *message)
 {
-  if (sms_count<1)
-  {
+ 
     //Begin serial communication with Arduino and SIM900
     mySerial.begin(9600);
   
@@ -68,13 +79,8 @@ void sendMessage(char *message)
     updateSerial();
     mySerial.write(26);
     printMe("SMS sent");
-    sms_count++;
-  }
-  else
-  {
-    printMe("SMS count++");
-  }
-  
+   
+ 
 }
 void updateSerial()
 {
@@ -93,40 +99,90 @@ void printMe(char *message)
 {
   lcd.setCursor(0,0);
      lcd.print(message);
-     lcd.setCursor(0,1);
-     lcd.print("......");
      delay(750);
      lcd.scrollDisplayLeft();//shifting data on LCD
      lcd.clear();
 }
 
+void red_blink()
+{
+    if(digitalRead(red_button)==HIGH)
+    {
+        digitalWrite(red_press, HIGH);
+    }
+    else
+    {
+        digitalWrite(red_press, LOW);  
+    }
+}
+
+void yellow_blink()
+{
+    if(digitalRead(yellow_button)==HIGH)
+    {
+        digitalWrite(yellow_press, HIGH);    
+    }
+    else
+    {
+        digitalWrite(yellow_press, LOW);  
+    }
+}
+void green_blink()
+{
+    if(digitalRead(green_button)==HIGH)
+    {
+        digitalWrite(green_press, HIGH);
+    }
+    else
+    {
+        digitalWrite(green_press, LOW);  
+    }
+  
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:  
+  red_blink();
+  yellow_blink();
+  green_blink();
   
   if (digitalRead (red_button) == HIGH && digitalRead(yellow_button)==LOW && digitalRead(green_button) == LOW)
   {
+      //digitalWrite(red_press, HIGH);
       digitalWrite(red, HIGH);
-      sms_count = 0;
       Serial.println("Red Button pressed..");
+      if (sms_count1<1 && digitalRead(red_button)== HIGH){
       sendMessage(red_str);  
+      sms_count1++; 
+        }
       printMe(red_str);
       delay(700);
   }
   else if (digitalRead (yellow_button) == HIGH && digitalRead(red_button) == HIGH && digitalRead(green_button)==LOW)
   {
+      
+      //digitalWrite(yellow_press, HIGH);
       digitalWrite(yellow, HIGH);
-      sms_count = 0; 
       Serial.println("Yellow Button pressed..");
-      sendMessage(yellow_str);  
+        if(sms_count2<1 && digitalRead(yellow_button)== HIGH)
+  {
+      sendMessage(yellow_str); 
+      sms_count2++;
+  } 
+        
       printMe(yellow_str);
       delay(700);
   }
   else if (digitalRead (green_button) == HIGH && digitalRead(red_button) == HIGH && digitalRead(yellow_button) == HIGH)
   {
+      //digitalWrite(green_press, HIGH);
       digitalWrite(green, HIGH);
-      sms_count = 0;
       Serial.println("Green Button pressed..");
-      sendMessage(green_str);  
+       if(sms_count3<1 && digitalRead(green_button)== HIGH)
+  {
+      sendMessage(green_str); 
+       sms_count3++;
+  }
       printMe(green_str);
       delay(700);
   }
@@ -135,8 +191,10 @@ void loop() {
       digitalWrite(red, LOW);
       digitalWrite(yellow, LOW);
       digitalWrite(green, LOW);
-      
-      Serial.println("No Input....");
+       sms_count3=0;
+      sms_count2=0;
+      sms_count1=0;
+      //Serial.println("No Input....");
       printMe(fine);
       delay(100);
       //lcd.clear();
